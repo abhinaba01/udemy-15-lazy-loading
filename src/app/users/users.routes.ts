4cb1c2338a99683ae01f139df8a732db99dfe0dc
abhinaba01 <abhinaba.mandal@capgemini.com>
2025-03-04 12:28:57 +0530

    Lazy Loading a service

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,12 +1,12 @@
 import { ResolveFn, Routes } from '@angular/router';
 
-
-import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
 import { inject } from '@angular/core';
 import { Task } from '../tasks/task/task.model';
 import { TasksService } from '../tasks/tasks.service';
-
-
 
 export const resolveUserTasks: ResolveFn<Task[]> = (
   activatedRouteSnapshot,
@@ -29,24 +29,30 @@ export const resolveUserTasks: ResolveFn<Task[]> = (
   return tasks.length ? tasks : [];
 };
 
-
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: () => import('../tasks/tasks.component').then((mod)=> mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+    providers:[TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        loadComponent: () =>
+          import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
