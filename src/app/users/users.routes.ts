776807b4406220413347fed2327c948fa6664b7e
abhinaba01 <abhinaba.mandal@capgemini.com>
2025-03-04 11:53:18 +0530

    Lazy Loading Resolve Fn

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,34 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 
-import { resolveUserTasks } from '../tasks/tasks.component';
+
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { inject } from '@angular/core';
+import { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+
+
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
 
 export const routes: Routes = [
   {
